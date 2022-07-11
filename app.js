d2246b51a53ecb713805c1e546e2a9d554c875be
EthanPittman171 <ethanpittman171@gmail.com>
2022-07-11 12:26:02 -0400

    Ensure that submit button must contain text content for a proper submission

--- app.js
+++ app.js
@@ -2,4 +2,19 @@ window.addEventListener('load', () => {
   const form = document.querySelector('#new-task-form');
   const input = document.querySelector('#new-task-input');
   const listElement = document.querySelector('#tasks');
+
+  //prevent a refresh of the page
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const task = input.value;
+    if (!task) {
+      alert("Please fill out the task input field.");
+      console.log("Failed task submission");
+      return;
+    } else {
+      console.log("Successful task submission");
+    }
+  });
+
 });
