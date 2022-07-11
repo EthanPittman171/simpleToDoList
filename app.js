d737dc8743b19342c71aabb0661c0416b37ae946
EthanPittman171 <ethanpittman171@gmail.com>
2022-07-11 12:48:05 -0400

    Allow user input to appear as text-only in the task section

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ window.addEventListener('load', () => {
   const input = document.querySelector('#new-task-input');
   const listElement = document.querySelector('#tasks');
 
-  //prevent a refresh of the page
+  //when a user attempts to submit a new task
   form.addEventListener('submit', (e) => {
+    //prevent refresh of page
     e.preventDefault();
 
+    /**
+     * If a user tries to submit a blank box, send an alert telling them to correct the issue.
+     * Otherwise, send a success message to the console.
+     */
     const task = input.value;
     if (!task) {
       alert("Please fill out the task input field.");
@@ -15,6 +20,34 @@ window.addEventListener('load', () => {
     } else {
       console.log("Successful task submission");
     }
+
+    //create a div that will contain the task text and associated buttons
+    const taskElement = document.createElement('div');
+    taskElement.classList.add('task');
+
+    //setup the actual content portion of the individual task item
+    const taskContentElement = document.createElement('div');
+    taskElement.classList.add('content');
+
+    //add the content/text portion of the individual task as a child to the task item in general
+    taskElement.appendChild(taskContentElement);
+
+    /**
+     * With the typed content from the user, add it as a literal input field
+     * inside of the task items. The point of this is to add the user text directly into the
+     * content div that was setup previously. Once setup, make this input field a child of 
+     * the content div.
+     */
+    const taskInputElement = document.createElement('input');
+    taskInputElement.classList.add('text');
+    taskInputElement.type = 'text';
+    taskInputElement.value = task;
+    taskInputElement.setAttribute('readonly', 'readonly');
+    taskContentElement.appendChild(taskInputElement);
+
+
+    //add the entire task item to the complete list of tasks
+    listElement.appendChild(taskElement);
   });
 
 });
